2f50540faa18ad687275ccf78b74c0d10acb067e
viniciuslevi <71188511+viniciuslevi@users.noreply.github.com>
2025-08-07 22:36:27 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Animação de digitação
+var typed = new Typed('#typed', {
+    strings: [
+        'Backend Developer',
+        'Software Embarcado',
+        'Criando soluções com C, C++, Java, Python',
+        'Amante de tecnologia'
+    ],
+    typeSpeed: 50,
+    backSpeed: 25,
+    loop: true
+});
+
+// Inicializar AOS (animações ao rolar)
+AOS.init();
